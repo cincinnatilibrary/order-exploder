7c53658ceffdff87e95bd99f26b9d6ad42fb4e5e
plchuser <ray.voelker@gmail.com>
2022-07-08 13:25:49 +0000

    new setup for order-exploder.plch.net

--- start_datasette.sh
+++ start_datasette.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/home/plchuser/output/order-exploder/venv/bin/python3 -m datasette serve /home/plchuser/output/order-exploder/my-app/ -h 127.0.0.1 -p 8014
+/home/plchuser/order-exploder/venv/bin/python3 -m datasette serve /home/plchuser/order-exploder/my-app/ -h 127.0.0.1 -p 8010
