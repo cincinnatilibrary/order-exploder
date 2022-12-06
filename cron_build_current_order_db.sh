20367b8b3f0aeb96d2fdf04969e5a067855b4a0f
plchuser <ray.voelker@gmail.com>
2022-12-06 11:56:48 -0500

    updating dev with current

--- cron_build_current_order_db.sh
+++ cron_build_current_order_db.sh
@@ -8,7 +8,7 @@
 
 # 0 7,11,15,17 * * 1,2,3,4,5	cd /home/plchuser/order-exploder/ ; ./cron_build_current_order_db.sh
 
-cd /home/plchuser/output/order-exploder/
+cd /home/plchuser/order-exploder/
 venv/bin/jupyter-nbconvert --to notebook --execute build_current_order_db.ipynb
 
 sudo /usr/bin/systemctl restart datasette.service
